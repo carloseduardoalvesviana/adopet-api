6ac3f9755b24330e592bd938d93b47fdb3357b9b
carloseduardoalvesviana <eduardostrink@gmail.com>
2025-12-31 00:03:09 -0300

    fix: Correctly retrieve PORT from environment variables to avoid default value

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import app from "./app";
 
-const PORTA = process.env || 3000;
+const PORTA = process.env.PORT || 3000;
 
 app.listen(PORTA, () => {
 	console.log(`Servidor executando em http://localhost:${PORTA}`);
